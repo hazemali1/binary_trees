e9d4b0519e64108c44905f414426075e07b39e01
hazemali1 <hazemali1343@gmail.com>
2023-08-31 16:04:42 -0700

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,16 +11,22 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int s = 0, d = 0;
 
+	if (tree == NULL)
+		return (0);
 	if (tree)
 	{
 		if (tree->left)
 		{
 			s = 1 + binary_tree_balance(tree->left);
 		}
+		else
+			s = 0;
 		if (tree->right)
 		{
 			d = 1 + binary_tree_balance(tree->right);
 		}
+		else
+			d = 0;
 	}
 	return (s - d);
 }
