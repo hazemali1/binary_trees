c905cf3852f2069d19755935ec9375819f463c05
hazemali1 <hazemali1343@gmail.com>
2023-08-31 15:56:34 -0700

    leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,11 +13,13 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left)
-		s++;
-	if (tree->right)
-		s++;
-	if (s == 0)
-		s++;
-	return (s);
+	if (tree)
+	{
+		s = binary_tree_leaves(tree->right);
+		d = binary_tree_leaves(tree->left);
+	}
+	if (s && d)
+		return (s + d);
+	else
+		return (1);
 }
