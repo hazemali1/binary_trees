50e6bfa46d23b5d2ebd7244014580b8bc48d4ca5
hazemali1 <hazemali1343@gmail.com>
2023-08-31 16:29:38 -0700

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,40 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_balanc - balance
+ *
+ * @tree: Tree
+ *
+ * Return: size of balance
+*/
+int binary_tree_balanc(const binary_tree_t *tree)
+{
+	int s = 0, d = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree)
+	{
+		if (tree->left)
+		{
+			s = 1 + binary_tree_balanc(tree->left);
+		}
+		else
+			s = 1;
+		if (tree->right)
+		{
+			d = 1 + binary_tree_balanc(tree->right);
+		}
+		else
+			d = 1;
+	}
+	if (s > d)
+		return (s);
+	else
+		return (d);
+}
+
+/**
  * binary_tree_balance - balance
  *
  * @tree: Tree
@@ -15,18 +49,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	if (tree)
 	{
-		if (tree->left)
-		{
-			s = 1 + binary_tree_balance(tree->left);
-		}
-		else
-			s = 1;
-		if (tree->right)
-		{
-			d = 1 + binary_tree_balance(tree->right);
-		}
-		else
-			d = 1;
+		s = binary_tree_balanc(tree->left);
+		d = binary_tree_balanc(tree->right);
 	}
 	return (s - d);
 }
