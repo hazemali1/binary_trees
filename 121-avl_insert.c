e87e21dabe24bc7a5c4c410b0a4d92c6547110c4
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-01 04:19:51 +0300

    insert

---
+++ 121-avl_insert.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - insert in bst
+ *
+ * @tree: Tree
+ * @value: Value n
+ *
+ * Return: pointer
+*/
+avl_t *avl_insert(avl_t **tree, int value)
+{
+	return (NULL);
+}
